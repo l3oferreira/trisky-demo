32d4f08d53dc1b9b92382950991574920bf90011
Carlos Heber <carlosheberpsantos@gmail.com>
2024-09-06 10:11:03 -0300

    V1 - Trisj=ky demo

---
+++ data/story.js
@@ -0,0 +1,768 @@
+var story = {
+ "docName": "VDI Tripla Connect",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "VDI Tripla Connect",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "438:403",
+   "groupIndex": 0,
+   "title": "Login ok",
+   "image": "login-ok.png",
+   "index": 0,
+   "width": 3445,
+   "height": 2208,
+   "x": 317,
+   "y": -3832,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Abre Dash link",
+     "rect": {
+      "x": 1464,
+      "y": 1356,
+      "width": 268,
+      "height": 157
+     },
+     "index": 0,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Abre Client link",
+     "rect": {
+      "x": 1732,
+      "y": 1356,
+      "width": 268,
+      "height": 157
+     },
+     "index": 1,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 0,
+       "frameIndex": 8,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "440:651",
+   "groupIndex": 0,
+   "title": "Login 2",
+   "image": "login-2.png",
+   "index": 1,
+   "width": 3445,
+   "height": 2208,
+   "x": 317,
+   "y": -1410,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null
+  },
+  {
+   "id": "317:2854",
+   "groupIndex": 0,
+   "title": "Dashboard 2",
+   "image": "dashboard-2.png",
+   "index": 2,
+   "width": 3200,
+   "height": 3200,
+   "x": 3980,
+   "y": -3857,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Group",
+     "rect": {
+      "x": 127.92529296875,
+      "y": 38.3291015625,
+      "width": 242.14903259277344,
+      "height": 69.32947540283203
+     },
+     "index": 2,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 73,
+      "y": 353,
+      "width": 216,
+      "height": 45
+     },
+     "index": 3,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 73,
+      "y": 453,
+      "width": 216,
+      "height": 45
+     },
+     "index": 4,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 81,
+      "y": 553,
+      "width": 274,
+      "height": 45
+     },
+     "index": 5,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 2,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "320:622",
+   "groupIndex": 0,
+   "title": "Geo IP",
+   "image": "geo-ip.png",
+   "index": 3,
+   "width": 3200,
+   "height": 3200,
+   "x": 7364,
+   "y": -3857,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Tabela",
+     "rect": {
+      "x": 1232,
+      "y": 2289,
+      "width": 1275,
+      "height": 457
+     },
+     "index": 6,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "Group",
+     "rect": {
+      "x": 127.92529296875,
+      "y": 38.3291015625,
+      "width": 242.14903259277344,
+      "height": 69.32947540283203
+     },
+     "index": 7,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 73,
+      "y": 253,
+      "width": 216,
+      "height": 45
+     },
+     "index": 8,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 73,
+      "y": 353,
+      "width": 216,
+      "height": 45
+     },
+     "index": 9,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      },
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 73,
+      "y": 453,
+      "width": 216,
+      "height": 45
+     },
+     "index": 10,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 81,
+      "y": 553,
+      "width": 274,
+      "height": 45
+     },
+     "index": 11,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 3,
+       "frameIndex": 3,
+       "disableAutoScroll": true,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "319:944",
+   "groupIndex": 0,
+   "title": "Dispositivos",
+   "image": "dispositivos.png",
+   "index": 4,
+   "width": 3200,
+   "height": 2350,
+   "x": 10728,
+   "y": -3857,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Tabela",
+     "rect": {
+      "x": 207,
+      "y": 964,
+      "width": 2919,
+      "height": 1097
+     },
+     "index": 12,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 5,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "logo",
+     "rect": {
+      "x": 48,
+      "y": 43,
+      "width": 49,
+      "height": 59
+     },
+     "index": 13,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "HOME",
+     "rect": {
+      "x": 53,
+      "y": 208,
+      "width": 226,
+      "height": 120
+     },
+     "index": 14,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 4,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "371:4605",
+   "groupIndex": 0,
+   "title": "VDIs",
+   "image": "vdis.png",
+   "index": 5,
+   "width": 3200,
+   "height": 2350,
+   "x": 14067,
+   "y": -3857,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Group",
+     "rect": {
+      "x": 127.92578125,
+      "y": 38.3291015625,
+      "width": 242.14903259277344,
+      "height": 69.32947540283203
+     },
+     "index": 15,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 73,
+      "y": 253,
+      "width": 216,
+      "height": 45
+     },
+     "index": 16,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 2,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 73,
+      "y": 353,
+      "width": 216,
+      "height": 45
+     },
+     "index": 17,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 5,
+       "disableAutoScroll": true,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 73,
+      "y": 453,
+      "width": 216,
+      "height": 45
+     },
+     "index": 18,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 4,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "nav",
+     "rect": {
+      "x": 81,
+      "y": 553,
+      "width": 274,
+      "height": 45
+     },
+     "index": 19,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 5,
+       "frameIndex": 3,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "352:210",
+   "groupIndex": 0,
+   "title": "Client Trisky 1",
+   "image": "client-trisky-1.png",
+   "index": 6,
+   "width": 3445,
+   "height": 2208,
+   "x": 18207,
+   "y": -3857,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "btn filtrar",
+     "rect": {
+      "x": 1081,
+      "y": 1015,
+      "width": 418,
+      "height": 123
+     },
+     "index": 20,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 7,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "tripla logo",
+     "rect": {
+      "x": 62.83984375,
+      "y": 34.98681640625,
+      "width": 126.16008758544922,
+      "height": 71.38343811035156
+     },
+     "index": 21,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 6,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "443:3547",
+   "groupIndex": 0,
+   "title": "Client Trisky 2",
+   "image": "client-trisky-2.png",
+   "index": 7,
+   "width": 3445,
+   "height": 2208,
+   "x": 21813,
+   "y": -3857,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "btn filtrar",
+     "rect": {
+      "x": 1081,
+      "y": 1015,
+      "width": 418,
+      "height": 123
+     },
+     "index": 22,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 8,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "tripla logo",
+     "rect": {
+      "x": 62.83984375,
+      "y": 34.98681640625,
+      "width": 126.16008758544922,
+      "height": 71.38343811035156
+     },
+     "index": 23,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 7,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  },
+  {
+   "id": "443:2651",
+   "groupIndex": 0,
+   "title": "Client Trisky 3",
+   "image": "client-trisky-3.png",
+   "index": 8,
+   "width": 3445,
+   "height": 2208,
+   "x": 25399,
+   "y": -3857,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "btn filtrar",
+     "rect": {
+      "x": 1074,
+      "y": 547,
+      "width": 428,
+      "height": 123
+     },
+     "index": 24,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 8,
+       "frameIndex": 6,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    },
+    {
+     "name": "tripla logo",
+     "rect": {
+      "x": 62.83984375,
+      "y": 34.98681640625,
+      "width": 126.16008758544922,
+      "height": 71.38343811035156
+     },
+     "index": 25,
+     "reactions": [
+      {
+       "action": "FRAME",
+       "navigationType": "NAVIGATE",
+       "trigger": "ON_CLICK",
+       "srcPageIndex": 8,
+       "frameIndex": 0,
+       "transAnimType": 0,
+       "transAnimDuration": 300,
+       "tmpDestModal": false
+      }
+     ]
+    }
+   ],
+   "layout": null
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 9,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "Page 1",
+   "backColor": "#FFFFFF"
+  }
+ ]
+}
